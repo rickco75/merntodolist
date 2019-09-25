da6fabc3e84653ab7cfa00bec68adcaf3a19ec3d
Rick Cohen <rickco@gmail.com>
2019-09-25 13:21:54 -0500

    finished todo front end

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 
 export default class CreateTodo extends Component {
 
@@ -41,10 +44,20 @@ export default class CreateTodo extends Component {
 
         // submit logic here
         console.log(`Form submitted:`);
-        console.log(`Todo Description ${this.state.todo_description}`);
-        console.log(`Todo Responsible ${this.state.todo_responsible}`);
-        console.log(`Todo Priority ${this.state.todo_priority}`);
-        console.log(`Todo Completed ${this.state.todo_completed}`);        
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+        console.log(`Todo Completed: ${this.state.todo_completed}`);        
+
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed  
+        }
+
+        axios.post('http://localhost:4000/todos/add',newTodo)
+             .then(res => console.log(res.data));
 
         this.setState({
             todo_description: '',
